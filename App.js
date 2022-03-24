fe79bf99c1766c14fd022e5c91606eea0e5ffc0e
João P. Meneguesso <jpm83633@gmail.com>
2022-03-24 19:11:00 -0300

    Adição do Nome da empresa
    Adicionei o nome da empresa, fiz alguns teses com a logo, alguns códigos comentados são para testes

--- App.js
+++ App.js
@@ -2,32 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import osCria from './assets/fotoDosCria.jpg';
 import ganes from './assets/logo.png';
+import nomeMarca from './assets/nomeMarca.png'
 
+// https://docs.expo.dev/ -->documentação do expo
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Image source={osCria} style={styles.crias} /> */}
-      <Image source={ganes} style={styles.crias} />
-      <Text>Missão</Text>
-      <Text>Visão</Text>
-      <Text>Valores</Text>
-      <StatusBar style="auto" />
-    </View>
+        <Image source={nomeMarca} style={{width: 'auto', height: 'auto',}} />
+        <Text>Missão</Text>
+        <Text>Visão</Text>
+        <Text>Valores</Text>
+        <StatusBar style="auto" />
+      </View>
+
+        // <View style={styles.containerLogo}>
+        //   <Image source={ganes} style={styles.logo} />
+        // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    width:50,
-    height:50,
-    marginLeft: 40,
   },
-  crias:{
+
+  // containerLogo: {
+  //   justifyContent:'center',
+  //   alignItems:'center',
+  // },
+
+  logo:{
     width:200,
-    height:200
+    height: 200
   }
 });
