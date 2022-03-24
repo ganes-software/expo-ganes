f0f78848729cc95b5285f6b63a550f7b55651d5f
João P. Meneguesso <jpm83633@gmail.com>
2022-03-24 11:14:23 -0300

    Alterações

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import osCria from './assets/fotoDosCria.jpg';
+import ganes from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Image source={osCria} style={styles.crias} /> */}
+      <Image source={ganes} style={styles.crias} />
+      <Text>Missão</Text>
+      <Text>Visão</Text>
+      <Text>Valores</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    width:50,
+    height:50,
+    marginLeft: 40,
   },
+  crias:{
+    width:200,
+    height:200
+  }
 });
