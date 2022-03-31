7cfc54b183329bc14f22c3bfb68cc0d1538353e4
João P. Meneguesso <jpm83633@gmail.com>
2022-03-30 22:20:13 -0300

    Código da tela de login

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
 import {Home} from './src/screens/home/index.js';
-import {Flat} from  './src/screens/flatlist/index.js'
+import {Flat} from './src/screens/flatlist/index.js'
+import {Login} from './src/screens/login/index.js'
 
 export default function App() {
   return (
+    // <Login />
     // <Home />
     <Flat />
   );
---
+++ src/screens/login/index.js
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import { Button, View, Image, StyleSheet,Text, TextInput } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { useFonts, ArchitectsDaughter_400Regular } from '@expo-google-fonts/architects-daughter';
+
+
+
+export function HomeScreen({navigation}) {
+   let [fontsLoaded] = useFonts({
+    ArchitectsDaughter_400Regular,
+  });
+
+  return (
+    
+    <View style={styles.sim} >
+    <Text style={styles.rikka}>{"LOGIN"}</Text>
+      <Text>{"\n"}</Text>
+        <Text>{"\n"}</Text>
+  
+              
+          <Image style={styles.logo} source={require('./assets/rikka-takanashi-takanashi-rikka.gif')}  />
+                <Text>{"\n"}</Text>
+                
+    <TextInput
+    style={{height: 40, width: 200, justifyContent: 'center' , color: '#000080'}}
+    placeholder="Digite seu Email"
+    />
+    
+    <TextInput
+    style={{height: 40, width: 200, justifyContent: 'center' , color: '#000080'}}
+    placeholder="Digite sua Senha"
+    />
+
+                <Text>{"\n"}</Text>
+      <Button 
+        onPress={() => navigation.navigate('Home')}
+        title="Logar"
+      />
+    </View>
+    
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+   let [fontsLoaded] = useFonts({
+    ArchitectsDaughter_400Regular,
+  });
+
+
+
+  return (
+    
+    <View style={styles.nao} >
+          <Image style={styles.logo} source={require('./assets/mai.gif')}  />
+                <Text>{"\n"}</Text>
+                <Text style={styles.rikka}>{"Você foi beneficiado com o poder da dúvida , parabéns!!!"}</Text>
+                <Text>{"\n"}</Text>
+     
+    </View>
+  );
+}
+
+function SobreScreen({ navigation }) {
+  return (
+    <View style={styles.maybe} >
+          <Image style={styles.logo} source={require('./assets/mirai.gif')}  />
+                <Text>{"\n"}</Text>
+                <Text style={styles.rikka}>{"Mirai Kuriyama"}</Text>
+                <Text>{"\n"}</Text>
+      <Button onPress={() => navigation.goBack()} title="Próximo" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+logo: {
+    height: 120,
+    width: 128,
+  },
+
+sim: {
+    backgroundColor: '#B0E0E6',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+
+nao: {
+    backgroundColor: '#F1D3FF',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  
+maybe: {
+    backgroundColor: '#FFDFDF',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+
+rikka: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontFamily: 'ArchitectsDaughter_400Regular',
+    fontSize: 25,
+  }
+  });
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+  
+      <Drawer.Navigator initialRouteName="Login">
+        <Drawer.Screen name="Login" component={HomeScreen} options={{ headerShown: false }} />
+        <Drawer.Screen name="Home" component={NotificationsScreen} />
+
+      </Drawer.Navigator>
+
+    </NavigationContainer>
+  );
+}
